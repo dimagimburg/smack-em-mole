76b4e01b4ce49634643c43e13916a350b53a7c18
Dima Gimburg <dimagimburg@Shays-MacBook-Pro.local>
2017-04-03 22:08:53 +0300

    Initial Commit

---
+++ SmackEmMole/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmackEmMole
+//
+//  Created by Dima Gimburg on 4/3/17.
+//  Copyright © 2017 Dima Gimburg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
